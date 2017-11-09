71d1c1156d0b999cad9addb63508c975aeaa01ac
Robert <torresrobert@me.com>
2017-11-09 14:27:55 -0500

    Initial Commit

---
+++ RatingTool/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  RatingTool
+//
+//  Created by Robert Torres on 11/9/17.
+//  Copyright © 2017 Robert Torres. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
